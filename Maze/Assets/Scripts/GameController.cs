5bff878de9271c2b136b6d86a085bfe81867320a
blesticles <robebm6@student.op.ac.nz>
2022-06-22 08:28:18 +1200

    uploading 4 games

---
+++ Maze/Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeConstructor))]           
+
+public class GameController : MonoBehaviour
+{
+    private MazeConstructor constructor;
+
+    [SerializeField] private int rows;
+
+    [SerializeField] private int cols;
+
+    public GameObject playerPrefab;
+
+    public GameObject monsterPrefab;
+
+    private AIController aIController;
+
+    void Awake()
+    {
+        constructor = GetComponent<MazeConstructor>();
+        aIController = GetComponent<AIController>(); 
+    }
+    
+    void Start()
+    {
+    
+        constructor.GenerateNewMaze(rows, cols, OnTreasureTrigger);
+
+        aIController.Graph = constructor.graph;
+        aIController.Player = CreatePlayer();
+        aIController.Monster = CreateMonster(); 
+        aIController.HallWidth = constructor.hallWidth;         
+        aIController.StartAI();
+    }
+
+    private GameObject CreatePlayer()
+    {
+        Vector3 playerStartPosition = new Vector3(constructor.hallWidth, 1, constructor.hallWidth);  
+        GameObject player = Instantiate(playerPrefab, playerStartPosition, Quaternion.identity);
+        player.tag = "Generated";
+
+        return player;
+    }
+
+    private GameObject CreateMonster()
+    {
+        Vector3 monsterPosition = new Vector3(constructor.goalCol * constructor.hallWidth, 0f, constructor.goalRow * constructor.hallWidth);
+        GameObject monster = Instantiate(monsterPrefab, monsterPosition, Quaternion.identity);
+        monster.tag = "Generated";  
+
+        return monster;  
+    }
+
+    private void OnTreasureTrigger(GameObject trigger, GameObject other)
+    { 
+        Debug.Log("You Won!");
+        aIController.StopAI();
+    }
+}
---
+++ Maze/Assets/Scripts/MazeConstructor.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+
+public class MazeConstructor : MonoBehaviour
+{
+    public bool showDebug;
+    
+    [SerializeField] private Material mazeMat1;
+    [SerializeField] private Material mazeMat2;
+    [SerializeField] private Material startMat;
+    [SerializeField] private Material treasureMat;
+
+    public float placementThreshold = 0.1f;   // chance of empty space
+
+    private MazeMeshGenerator meshGenerator;
+
+    public int[,] data
+    {
+        get; private set;
+    }
+
+    public float hallWidth{ get; private set; }
+    public int goalRow{ get; private set; }
+    public int goalCol{ get; private set; }
+
+    public Node[,] graph;
+
+    void Awake()
+    {
+        // default to walls surrounding a single empty cell
+        data = new int[,]
+        {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+        meshGenerator = new MazeMeshGenerator();
+        hallWidth = meshGenerator.width;
+    }
+        private void DisplayMaze()
+        {
+            GameObject go = new GameObject();
+            go.transform.position = Vector3.zero;
+            go.name = "Procedural Maze";
+            go.tag = "Generated";
+
+            MeshFilter mf = go.AddComponent<MeshFilter>();
+            mf.mesh = meshGenerator.FromData(data);
+            
+            MeshCollider mc = go.AddComponent<MeshCollider>();
+            mc.sharedMesh = mf.mesh;
+
+            MeshRenderer mr = go.AddComponent<MeshRenderer>();
+            mr.materials = new Material[2] {mazeMat1, mazeMat2};
+        }
+    
+
+        void OnGUI()
+    {
+        if (!showDebug)
+            return;
+
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        string msg = "";
+
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = 0; j <= cMax; j++)
+                msg += maze[i, j] == 0 ? "...." : "==";
+            msg += "\n";
+        }
+
+        GUI.Label(new Rect(20, 20, 500, 500), msg);
+    }
+
+        public void GenerateNewMaze(int sizeRows, int sizeCols, TriggerEventHandler treasureCallback)
+    {
+        DisposeOldMaze();  
+
+        if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
+            Debug.LogError("Odd numbers work better for dungeon size.");
+
+        data = FromDimensions(sizeRows, sizeCols);
+
+        graph = new Node[sizeRows,sizeCols];
+
+        for (int i = 0; i < sizeRows; i++)        
+            for (int j = 0; j < sizeCols; j++)            
+                graph[i, j] = data[i,j] == 0 ? new Node(i, j, true) : new Node(i, j, false);
+
+        goalRow = data.GetUpperBound(0) - 1;
+        goalCol = data.GetUpperBound(1) - 1;
+
+        DisplayMaze();
+
+        PlaceGoal(treasureCallback); 
+    }
+
+        public int[,] FromDimensions(int sizeRows, int sizeCols)
+    {
+                int[,] maze = new int[sizeRows, sizeCols];
+                int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)        
+            for (int j = 0; j <= cMax; j++)            
+                if (i == 0 || j == 0 || i == rMax || j == cMax)                
+                    maze[i, j] = 1;                                    
+                else if (i % 2 == 0 && j % 2 == 0 && Random.value > placementThreshold)                                    
+                {
+                    maze[i, j] = 1;
+
+                    int a = Random.value < .5 ? 0 : (Random.value < .5 ? -1 : 1);
+                    int b = a != 0 ? 0 : (Random.value < .5 ? -1 : 1);
+                    maze[i+a, j+b] = 1;
+                }  
+
+        return maze;
+    }
+
+    public void DisposeOldMaze()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Generated");
+        foreach (GameObject go in objects) {
+            Destroy(go);
+        }
+    }
+
+    private void PlaceGoal(TriggerEventHandler treasureCallback)
+    {            
+        GameObject treasure = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        treasure.transform.position = new Vector3(goalCol * hallWidth, .5f, goalRow * hallWidth);
+        treasure.name = "Treasure";
+        treasure.tag = "Generated";
+
+        treasure.GetComponent<BoxCollider>().isTrigger = true;
+        treasure.GetComponent<MeshRenderer>().sharedMaterial = treasureMat;
+
+        TriggerEventRouter tc = treasure.AddComponent<TriggerEventRouter>();
+        tc.callback = treasureCallback;
+    }
+}
